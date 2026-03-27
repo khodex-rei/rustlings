ceddaccfc52e44b0b23ec59d1b9ab6ab4235c1a0
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2026-03-27 08:13:59 +0000

    Completed Exercise 7

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -36,6 +36,16 @@ mod tests {
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
 
+        let your_order = Order {
+            name: "Hacker in Rust".to_string(),
+            count: 1,
+            item_number: 123,
+            year: 2019,
+            made_by_email: true,
+            made_by_mobile: false,
+            made_by_phone: false,
+        };
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
